c46b99d9632671f65a7748c3db9c84c1715d4ff0
LeKo <leopardorocha@gmail.com>
2024-07-10 15:01:53 -0300

    ADD

--- resources/views/dashboard/admin_dashboard.blade.php
+++ resources/views/dashboard/admin_dashboard.blade.php
@@ -163,7 +163,7 @@
                 <!-- Card Header - Dropdown -->
                 <div
                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Renda Mensal (EM PRODUÇÃO) </h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Renda Mensal</h6>
                     <div class="dropdown no-arrow">
                         <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
